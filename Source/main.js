bd13e70f4319c6d0a02eb202d752c773db828ede
Daniel Glazer <danielglazer123@gmail.com>
2017-08-13 23:39:33 +0300

    commit 6
    working DND(drag N drop)

--- Source/main.js
+++ Source/main.js
@@ -1,4 +1,34 @@
- <script>
-    <script src='../Libraries/d3/d3.js'></script>
-    <script src='../Libraries/bootstrap-4.0.0-beta-dist/js/bootstrap.js'></script>
- </script>
+// A $( document ).ready() block.
+$(document).ready(function () {
+    console.log("ready!");
+    function dropJSON(targetEl, callback) {
+        // disable default drag & drop functionality
+        targetEl.addEventListener('dragenter', function (e) { e.preventDefault(); });
+        targetEl.addEventListener('dragover', function (e) { e.preventDefault(); });
+
+        targetEl.addEventListener('drop', function (event) {
+
+            var reader = new FileReader();
+            reader.onloadend = function () {
+                var data = JSON.parse(this.result);
+                callback(data);
+            };
+
+            reader.readAsText(event.dataTransfer.files[0]);
+            event.preventDefault();
+
+        });
+    }
+
+    var x = document.getElementById("DragAndDrop");
+
+    dropJSON(
+        document.getElementById("DragAndDrop"),
+        function (data) {
+            // dropped - do something with data
+            console.log(data);
+        }
+    );
+});
+
+
