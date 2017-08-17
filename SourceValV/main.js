f1e2d714c47820bffd0e60bbccf11fef15af3ce1
firluk <firluk@gmail.com>
2017-08-17 22:12:59 +0300

    commit11
    experiments on leaflet
    layer

--- SourceValV/main.js
+++ SourceValV/main.js
@@ -1,112 +1,168 @@
 'use strict';
 
 // remove this if you use Modernizr 
-(function (e, t, n) { var r = e.querySelectorAll("html")[0];
-r.className = r.className.replace(/(^|\s)no-js(\s|$)/, "$1js$2") })(document, window, 0);
+(function (e, t, n) {
+    var r = e.querySelectorAll("html")[0];
+    r.className = r.className.replace(/(^|\s)no-js(\s|$)/, "$1js$2")
+})(document, window, 0);
 
-function isSuccess(data){
-  return data.type == "FeatureCollection";
-};
-
+var fileData;
 // A $( document ).ready() block.
 $(document).ready(function () {
-  console.log("ready!");
-  
-  (function (document, window, index) {
-    // feature detection for drag&drop upload
-    var isAdvancedUpload = function () {
-        var div = document.createElement('div');
-        return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
-    }();
+    console.log("ready!");
+
+    (function (document, window, index) {
+        // feature detection for drag&drop upload
+        var isAdvancedUpload = function () {
+            var div = document.createElement('div');
+            return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
+        }();
 
 
-    // applying the effect for every form
-    var forms = document.querySelectorAll('.box');
-    Array.prototype.forEach.call(forms, function (form) {
-        var input = form.querySelector('input[type="file"]'),
-            label = form.querySelector('label'),
-            errorMsg = form.querySelector('.box__error span'),
-            restart = form.querySelectorAll('.box__restart'),
-            droppedFiles = false,
-            showFiles = function (files) {
-                label.textContent = files.length > 1 ? (input.getAttribute('data-multiple-caption') || '').replace('{count}', files.length) : files[0].name;
-            },
-            triggerFormSubmit = function () {
-                var event = document.createEvent('HTMLEvents');
-                event.initEvent('submit', true, false);
-                form.dispatchEvent(event);
-            };
+        // applying the effect for every form
+        var forms = document.querySelectorAll('.box');
+        Array.prototype.forEach.call(forms, function (form) {
+            var input = form.querySelector('input[type="file"]'),
+                label = form.querySelector('label'),
+                errorMsg = form.querySelector('.box__error span'),
+                restart = form.querySelectorAll('.box__restart'),
+                droppedFiles = false,
+                showFiles = function (files) {
+                    label.textContent = files.length > 1 ? (input.getAttribute('data-multiple-caption') || '').replace('{count}', files.length) : files[0].name;
+                },
+                triggerFormSubmit = function () {
+                    var event = document.createEvent('HTMLEvents');
+                    event.initEvent('submit', true, false);
+                    form.dispatchEvent(event);
+                };
 
-        // automatically submit the form on file select
-        input.addEventListener('change', function (e) {
-            showFiles(e.target.files);
-            triggerFormSubmit();
-        });
-
-        // drag&drop files if the feature is available
-        if (isAdvancedUpload) {
-            form.classList.add('has-advanced-upload'); // letting the CSS part to know drag&drop is supported by the browser
-
-            ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach(function (event) {
-                form.addEventListener(event, function (e) {
-                    // preventing the unwanted behaviours
-                    e.preventDefault();
-                    e.stopPropagation();
-                });
-            });
-            ['dragover', 'dragenter'].forEach(function (event) {
-                form.addEventListener(event, function () {
-                    form.classList.add('is-dragover');
-                });
-            });
-            ['dragleave', 'dragend', 'drop'].forEach(function (event) {
-                form.addEventListener(event, function () {
-                    form.classList.remove('is-dragover');
-                });
-            });
-            form.addEventListener('drop', function (e) {
-                droppedFiles = e.dataTransfer.files; // the files that were dropped
-                showFiles(droppedFiles);
+            // automatically submit the form on file select
+            input.addEventListener('change', function (e) {
+                showFiles(e.target.files);
                 triggerFormSubmit();
-
             });
-        }
+
+            // drag&drop files if the feature is available
+            if (isAdvancedUpload) {
+                form.classList.add('has-advanced-upload'); // letting the CSS part to know drag&drop is supported by the browser
+
+                ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach(function (event) {
+                    form.addEventListener(event, function (e) {
+                        // preventing the unwanted behaviours
+                        e.preventDefault();
+                        e.stopPropagation();
+                    });
+                });
+                ['dragover', 'dragenter'].forEach(function (event) {
+                    form.addEventListener(event, function () {
+                        form.classList.add('is-dragover');
+                    });
+                });
+                ['dragleave', 'dragend', 'drop'].forEach(function (event) {
+                    form.addEventListener(event, function () {
+                        form.classList.remove('is-dragover');
+                    });
+                });
+                form.addEventListener('drop', function (e) {
+                    droppedFiles = e.dataTransfer.files; // the files that were dropped
+                    showFiles(droppedFiles);
+                    triggerFormSubmit();
+
+                });
+            }
 
 
-        // if the form was submitted
-        form.addEventListener('submit', function (e) {
-            // preventing the duplicate submissions if the current one is in progress
-            if (form.classList.contains('is-uploading')) return false;
+            // if the form was submitted
+            form.addEventListener('submit', function (e) {
+                // preventing the duplicate submissions if the current one is in progress
+                if (form.classList.contains('is-uploading')) return false;
 
-            form.classList.add('is-uploading');
-            form.classList.remove('is-error');
+                form.classList.add('is-uploading');
+                form.classList.remove('is-error');
 
-            var reader = new FileReader();
-            reader.onloadend = function () {
-                form.classList.remove('is-uploading');
-                var data = JSON.parse(this.result);
-                form.classList.add(isSuccess(data) == true ? 'is-success' : 'is-error');
-                console.log(data);
-            };
+                var reader = new FileReader();
+                reader.onloadend = function () {
+                    form.classList.remove('is-uploading');
+                    var data = JSON.parse(this.result);
+                    form.classList.add(isSuccess(data) == true ? 'is-success' : 'is-error');
+                    console.log(data);
+                    fileData = data;
+                    visualize();
+                };
 
-            reader.readAsText(droppedFiles[0]);
-            event.preventDefault();
+                reader.readAsText(droppedFiles[0]);
+                event.preventDefault();
+            });
+
+
+            // restart the form if has a state of error/success
+            Array.prototype.forEach.call(restart, function (entry) {
+                entry.addEventListener('click', function (e) {
+                    e.preventDefault();
+                    form.classList.remove('is-error', 'is-success');
+                    input.click();
+                });
+            });
+
+            // Firefox focus bug fix for file input
+            input.addEventListener('focus', function () { input.classList.add('has-focus'); });
+            input.addEventListener('blur', function () { input.classList.remove('has-focus'); });
         });
 
+    }(document, window, 0));
+});
 
-        // restart the form if has a state of error/success
-        Array.prototype.forEach.call(restart, function (entry) {
-            entry.addEventListener('click', function (e) {
-                e.preventDefault();
-                form.classList.remove('is-error', 'is-success');
-                input.click();
-            });
-        });
+function isSuccess(data) {
+    return data.type == "FeatureCollection";
+};
 
-        // Firefox focus bug fix for file input
-        input.addEventListener('focus', function () { input.classList.add('has-focus'); });
-        input.addEventListener('blur', function () { input.classList.remove('has-focus'); });
+function visualize() {
+
+    var map = L.map('mapid');
+    map.createPane('labels');
+    map.getPane('labels').style.zIndex = 650;
+    map.getPane('labels').style.pointerEvents = 'none';
+    var positron = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+        attribution: '©OpenStreetMap, ©CartoDB'
+    }).addTo(map);
+
+    var positronLabels = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', {
+        attribution: '©OpenStreetMap, ©CartoDB',
+        pane: 'labels'
+    }).addTo(map);
+    var geojson = L.geoJson(fileData).addTo(map);
+    geojson.eachLayer(function (layer) {
+        layer.bindPopup(layer.feature.properties.name);
     });
 
-}(document, window, 0));
-});
+    map.fitBounds(geojson.getBounds());
+    
+    //    var continents = d3.nest()
+    //    .key(function(d) { return d.properties.continent;})
+    //    .rollup(function(v) { return d3.sum(v, function(d) { return d.properties.country_active; }); })
+    //    .entries(fileData.features);
+    //    //console.log(continents);
+    //     console.log(continents);
+
+    //   var economies = d3.nest()
+    //    .key(function(d) { return d.properties.economy;})
+    //    .entries(fileData.features);
+    //    //console.log(continents);
+    //     console.log(economies);
+
+
+    //     var mymap = L.map('mapid').setView([0, 0], 2);
+    //     L.tileLayer.provider('Esri.WorldGrayCanvas').addTo(mymap);
+    //     var countriesLayer = L.geoJson(fileData).addTo(mymap);
+
+
+    //     mymap.fitBounds(countriesLayer);
+    // L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    //     minZoom: 2,    
+    //     maxZoom: 4,
+    //     id: 'mapbox.streets',
+    //     style: 'mapbox://styles/mapbox/light-v9',
+    //     accessToken: 'pk.eyJ1IjoidmlzdWFsaXphdGlvbiIsImEiOiJjajZldGJhN3AyamRwMnFsczdlZTc1bnV3In0.kWBNVk-R38vQ1mazvFrB6A'
+    //     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+    // }).addTo(mymap);
+};
