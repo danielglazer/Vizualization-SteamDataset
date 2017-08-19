e46b388f7ffcfce8522db22e169d4f3982e677a6
firluk <firluk@gmail.com>
2017-08-19 13:30:23 +0300

    commit20
    control dials mockup
    style, legend and info change according to chosen property
    dictionary mockup

--- Code/VisualizingData/SourceValV/main.js
+++ Code/VisualizingData/SourceValV/main.js
@@ -189,10 +189,86 @@ function pickForCurrentProperty(d) {
     return d.properties[currentProperty];
 }
 
-function translate_to_UI(property_name) {
-
-
-}
+var dictionary = {
+    "all_num_active": "Active players worldwide",
+    "all_num_owners": "Game owners worldwide",
+    "avg_play_time": "Average playtime worldwide",
+    "continent": "Continent",
+    "country_active": "Active players in country",
+    "country_owners": "Game owners in country",
+    "game1active_users": "Dota 2 | Active gamers",
+    "game1avg_play_time": "Dota 2 | Playtime in 2 weeks",
+    "game1casual_users": "Dota 2 | Casual gamers ",
+    "game1excessive_users": "Dota 2 | Excessive gamers ",
+    "game1moderate_users": "Dota 2 | Moderate gamers",
+    "game1owners": "Dota 2 | Owners",
+    "game2active_users": "Counter-Strike: Global Offensive | Active gamers",
+    "game2avg_play_time": "Counter-Strike: Global Offensive | Playtime in 2 weeks",
+    "game2casual_users": "Counter-Strike: Global Offensive | Casual gamers ",
+    "game2excessive_users": "Counter-Strike: Global Offensive | Excessive gamers ",
+    "game2moderate_users": "Counter-Strike: Global Offensive | Moderate gamers",
+    "game2owners": "Counter-Strike: Global Offensive | Owners",
+    "game3active_users": "Team Fortress 2 | Active gamers",
+    "game3avg_play_time": "Team Fortress 2 | Playtime in 2 weeks",
+    "game3casual_users": "Team Fortress 2 | Casual gamers ",
+    "game3excessive_users": "Team Fortress 2 | Excessive gamers ",
+    "game3moderate_users": "Team Fortress 2 | Moderate gamers",
+    "game3owners": "Team Fortress 2 | Owners",
+    "game4active_users": "Garry's Mod | Active gamers",
+    "game4avg_play_time": "Garry's Mod | Playtime in 2 weeks",
+    "game4casual_users": "Garry's Mod | Casual gamers ",
+    "game4excessive_users": "Garry's Mod | Excessive gamers ",
+    "game4moderate_users": "Garry's Mod | Moderate gamers",
+    "game4owners": "Garry's Mod | Owners",
+    "game5active_users": "Skyrim | Active gamers",
+    "game5avg_play_time": "Skyrim | Playtime in 2 weeks",
+    "game5casual_users": "Skyrim | Casual gamers ",
+    "game5excessive_users": "Skyrim | Excessive gamers " ,
+    "game5moderate_users": "Skyrim | Moderate gamers",
+    "game5owners": "Skyrim | Owners",
+    "game6active_users": "Rust | Active gamers",
+    "game6avg_play_time": "Rust | Playtime in 2 weeks",
+    "game6casual_users": "Rust | Casual gamers ",
+    "game6excessive_users": "Rust | Excessive gamers ",
+    "game6moderate_users": "Rust | Moderate gamers",
+    "game6owners": "Rust | Owners",
+    "game7active_users": "Sid Meier\'s Civilization® V | Active gamers",
+    "game7avg_play_time": "Sid Meier\'s Civilization® V | Playtime in 2 weeks",
+    "game7casual_users": "Sid Meier\'s Civilization® V | Casual gamers ",
+    "game7excessive_users": "Sid Meier\'s Civilization® V | Excessive gamers ",
+    "game7moderate_users": "Sid Meier\'s Civilization® V | Moderate gamers",
+    "game7owners": "Sid Meier\'s Civilization® V | Owners",
+    "game8active_users": "Terraria | Active gamers",
+    "game8avg_play_time": "Terraria | Playtime in 2 weeks",
+    "game8casual_users": "Terraria | Casual gamers ",
+    "game8excessive_users": "Terraria | Excessive gamers ",
+    "game8moderate_users": "Terraria | Moderate gamers",
+    "game8owners": "Terraria | Owners",
+    "game9active_users": "DayZ | Active gamers",
+    "game9avg_play_time | Playtime in 2 weeks": "DayZ",
+    "game9casual_users": "DayZ | Casual gamers ",
+    "game9excessive_users": "DayZ | Excessive gamers ",
+    "game9moderate_users": "DayZ | Moderate gamers",
+    "game9owners": "DayZ | Owners",
+    "game10active_users": "PAYDAY 2 | Active gamers",
+    "game10avg_play_time": "PAYDAY 2 | Playtime in 2 weeks",
+    "game10casual_users": "PAYDAY 2 | Casual gamers ",
+    "game10excessive_users": "PAYDAY 2 | Excessive gamers ",
+    "game10moderate_users": "PAYDAY 2 | Moderate gamers",
+    "game10owners": "PAYDAY 2 | Owners",
+    "gdp_md_est": "GDP Median Estimate in millions of $",
+    "income_grp": "Country's Income Group",
+    "iso_a2": "2-letter country code",
+    "iso_a3": "3-letter country code",
+    "money_spent": "Money spent for buying games",
+    "name": "Country Name",
+    "name_long": "Country long Name",
+    "num_casual_users": "Casual gamers (all games)",
+    "num_excessive_users": "Excessive gamers (all games)",
+    "num_moderate_users": "Moderate gamers (all games)",
+    "pop_est": "Population",
+    "subregion": "Geographical Subregion"
+};
 
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -226,10 +302,23 @@ function visualize() {
 
     var color = d3.scaleLinear().domain([0, d3.max(fileData.features, function (d) {
         return pickForCurrentProperty(d)
-    }
-    )])
+    })])
         .interpolate(d3.interpolateHcl)
         .range([d3.rgb("#f7fbff"), d3.rgb('#08306b')]);
+
+    function regenColor(color1, color2) {
+        if (!color1) {
+            color1 = "#f7fbff";
+        }
+        if (!color2) {
+            color2 = '#08306b';
+        }
+        color = d3.scaleLinear().domain([0, d3.max(fileData.features, function (d) {
+            return pickForCurrentProperty(d)
+        })])
+            .interpolate(d3.interpolateHcl)
+            .range([d3.rgb(color1), d3.rgb(color2)]);
+    }
 
     function style(feature) {
         return {
@@ -238,7 +327,7 @@ function visualize() {
             opacity: 1,
             color: 'black',
             dashArray: '3',
-            fillOpacity: 0.9
+            fillOpacity: 1
         };
     }
 
@@ -254,10 +343,11 @@ function visualize() {
     info.update = function (feature) {
         this._div.innerHTML = (feature ?
             '<b>' + feature.properties.name + '</b><br />' +
-            '<b>' + currentProperty + '</b><br />' +
+            '<b>' + dictionary[currentProperty] + '</b><br />' +
             '<svg width="10" height="10"><rect width="10" height="10"style="fill:' + color(pickForCurrentProperty(feature)) + ';stroke-width:1;stroke:rgb(0,0,0)"/></svg> ' +
             numberWithCommas(pickForCurrentProperty(feature)) + '<br />'
-            : 'Hover over a country');
+            :'Hover over a country </br>'+ 
+            '<b>' + dictionary[currentProperty] + '</b><br />');
     };
     info.addTo(map);
 
@@ -320,21 +410,60 @@ function visualize() {
     // legend
     var legend = L.control({ position: 'bottomright' });
     legend.onAdd = function (map) {
+        this._div = L.DomUtil.create('div', 'info legend')
+        legend.update();
+        return this._div;
+    };
+    legend.update = function () {
         var max = d3.max(fileData.features, function (d) {
             return pickForCurrentProperty(d)
         });
-        var div = L.DomUtil.create('div', 'info legend'),
-            grades = [0, Math.floor((1/4)*max), Math.floor((2/4)*max), Math.floor((3/4)*max), Math.floor(max)],
+        var grades = [0, Math.floor((1 / 4) * max), Math.floor((2 / 4) * max), Math.floor((3 / 4) * max), Math.floor(max)],
             labels = [];
-
+        this._div.innerHTML = "";
         // loop through our density intervals and generate a label with a colored square for each interval
         for (var i = 0; i < grades.length; i++) {
-            div.innerHTML +=
+            this._div.innerHTML +=
                 '<i style="background:' + color(grades[i]) + '"></i> ' +
                 numberWithCommas(grades[i]) + '<br>';
         }
-        return div;
-    };
+    }
     legend.addTo(map);
 
+    var propertyCtrl = L.control({ position: 'bottomleft' });
+    propertyCtrl.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'info propertyCtrl');
+
+        var jdiv = $(div);
+        var form = $("<form></form>");
+
+        if (fileData.features.length > 1) {
+            var props = Object.keys(fileData.features[0].properties);
+            props.sort();
+
+            for (var i = 0; i < props.length; i++) {
+                var radioBtn = $('<input type="radio" name="property_"' + props[i] + '" value=' + props[i] + ' />');
+                radioBtn.on('change', function (e) {
+                    console.log(this.value);
+                    currentProperty = this.value;
+                    updateMap();
+                });
+                form.append(radioBtn);
+            }
+
+            jdiv.append(form);
+        }
+
+        return jdiv[0];
+    }
+    propertyCtrl.addTo(map);
+
+    function updateMap() {
+        regenColor();
+        info.update();
+        legend.update();
+        geojson.eachLayer(function (layer) {
+            geojson.resetStyle(layer);
+        });
+    }
 };
